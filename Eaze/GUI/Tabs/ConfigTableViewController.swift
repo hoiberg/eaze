9b3a6a05324af1da66d92d568dfbf82e1038b17a
hoiberg <avdlugt@xs4all.nl>
2016-07-18 10:32:17 +0200

    Config icons

--- Eaze/GUI/Tabs/ConfigTableViewController.swift
+++ Eaze/GUI/Tabs/ConfigTableViewController.swift
@@ -5,9 +5,6 @@
 //  Created by Alex on 22-11-15.
 //  Copyright © 2015 Hangar42. All rights reserved.
 //
-//   Note: Because of a bug in iOS we can only use the splitViewController on iPads. This means we have to use
-//   replace segues on an iPad and push segues on iPhones. To keep the storyboard clean we do this in code.
-//   Hence the storyboardNames array..
 
 import UIKit
 
@@ -15,22 +12,44 @@ final class ConfigTableViewController: UITableViewController {
     
     // MARK: - Variables
     
-    private let storyboardNames = [["General", "Receiver", "Motors", "Serial", "ReceiverInput"], ["AppPrefs", "AppLog", "AboutApp"]]
-    private var currentSelection = (section: 99, row: 99),
-                isLoading = false
+    private let cellTitles  = [["General", "Receiver", "ESC/Motors", "Serial Ports", "Receiver Input"],
+                               ["App Preferences", "Developer Console", "About This App"]],
+                sectionTitles = ["Cleanflight", "App"],
+                identifiers = [["General", "Receiver", "Motors", "Serial", "ReceiverInput"],
+                               ["AppPrefs", "AppLog", "AboutApp"]]
+    
+    private var isLoading = false
     
     
     // MARK: - Functions
     
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return identifiers.count
+    }
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return identifiers[section].count
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("Cell")!
+        cell.textLabel?.text = cellTitles[indexPath.section][indexPath.row]
+        cell.imageView?.image = UIImage(named: "Config-" + identifiers[indexPath.section][indexPath.row])
+        cell.imageView?.layer.cornerRadius = 4.0
+        cell.imageView?.clipsToBounds = true
+        
+        return cell
+    }
+    
     override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
         tableView.deselectRowAtIndexPath(indexPath, animated: true)
-                
+        
         guard !isLoading else { return }
         isLoading = true
         
         // load vc async for better performance
         dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0)) {
-            let vc = self.storyboard!.instantiateViewControllerWithIdentifier(self.storyboardNames[indexPath.section][indexPath.row])
+            let vc = self.storyboard!.instantiateViewControllerWithIdentifier(self.identifiers[indexPath.section][indexPath.row])
             
             dispatch_async(dispatch_get_main_queue()) {
                 self.isLoading = false
@@ -41,5 +60,9 @@ final class ConfigTableViewController: UITableViewController {
                 }
             }
         }
+    }
+    
+    override func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return sectionTitles[section]
     }
 }
