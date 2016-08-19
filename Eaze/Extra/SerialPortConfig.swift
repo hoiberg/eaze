9232ce114a1f5c88a07205ad4efba90185536e94
hoiberg <avdlugt@xs4all.nl>
2016-08-19 12:22:15 +0200

    Changed to copyable class + cleanup

--- Eaze/Extra/SerialPortConfig.swift
+++ Eaze/Extra/SerialPortConfig.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-struct SerialPortConfig {
+class SerialPortConfig: Copyable {
     
     private static let portIdentifierToNameMapping = [0: "UART1", 1: "UART2", 2: "UART3", 3: "UART4", 20: "USB VCP", 30: "SOFTSERIAL1", 31: "SOFTSERIAL2"]
     
@@ -20,39 +20,50 @@ struct SerialPortConfig {
     var GPS_baudrate = Baudrate.Auto
     var TELEMETRY_baudrate = Baudrate.Auto
     var BLACKBOX_baudrate = Baudrate.Auto
+    
+    init() {}
+    
+    required init(copy: SerialPortConfig) {
+        identifier = copy.identifier
+        functions = copy.functions
+        MSP_baudrate = copy.MSP_baudrate
+        GPS_baudrate = copy.GPS_baudrate
+        TELEMETRY_baudrate = copy.TELEMETRY_baudrate
+        BLACKBOX_baudrate = copy.BLACKBOX_baudrate
+    }
 }
 
 
 enum SerialPortFunction: Int {
-    // TELM_MSP only < 1.15.0, TELM_LTM only >= 1.15.0 (id TELM_LTM == id TELM_MSP, TELM_LTM replaced TELM_MSP), MAVLINK only >= 1.18.0
-    case MSP, GPS, TELEMETRY_FRSKY, TELEMETRY_HOTT, TELEMETRY_MSP_LTM, TELEMETRY_SMARTPORT, RX_SERIAL, BLACKBOX, TELEMETRY_MAVLINK
+    case MSP,
+         GPS,
+         TELEMETRY_FRSKY,
+         TELEMETRY_HOTT,
+         TELEMETRY_MSP_LTM, // MSP < 1.15.0, replaced by LTM >= 1.15.0, same ID
+         TELEMETRY_SMARTPORT,
+         RX_SERIAL, BLACKBOX,
+         TELEMETRY_MAVLINK // >= 1.18.0
     
     static var all: [SerialPortFunction] {
-        get {
-            return [MSP, GPS, TELEMETRY_FRSKY, TELEMETRY_HOTT, TELEMETRY_MSP_LTM, TELEMETRY_SMARTPORT, RX_SERIAL, BLACKBOX, TELEMETRY_MAVLINK]
-        }
+        return [MSP, GPS, TELEMETRY_FRSKY, TELEMETRY_HOTT, TELEMETRY_MSP_LTM, TELEMETRY_SMARTPORT, RX_SERIAL, BLACKBOX, TELEMETRY_MAVLINK]
     }
 }
+
+//SerialPortFunction.RawValue
 
 
 enum Baudrate: Int {
     case Auto, B9600, B19200, B38400, B57600, B115200, B230400, B250000
     
     var name: String {
-        get {
-            return ["AUTO", "9600", "19200", "38400", "57600", "115200", "230400", "250000"][rawValue]
-        }
+        return ["AUTO", "9600", "19200", "38400", "57600", "115200", "230400", "250000"][rawValue]
     }
     
     var intValue: Int {
-        get {
-            return [0, 9600, 19200, 38400, 57600, 115200, 230400, 250000][rawValue]
-        }
+        return [0, 9600, 19200, 38400, 57600, 115200, 230400, 250000][rawValue]
     }
     
     static var all: [Baudrate] {
-        get {
-            return [Auto, B9600, B19200, B38400, B57600, B115200, B230400, B250000]
-        }
+        return [Auto, B9600, B19200, B38400, B57600, B115200, B230400, B250000]
     }
 }
